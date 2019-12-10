522c657e84069b43337f598d4a61bd288fa8cd04
Mikkel <MikkelHansen95@live.dk>
2019-12-10 09:01:53 +0100

    updated frontend

--- frontend/index.js
+++ frontend/index.js
@@ -12,7 +12,6 @@ async function getCatering() {
             return res.json();
         })
         .then(caterings => {
-            console.log(caterings)
             var cateringDOM = document.getElementById("catering");
             cateringDOM.innerHTML = ""
             caterings.forEach((catering) => {
@@ -33,7 +32,6 @@ async function getFacilities() {
             return res.json();
         })
         .then(facilities => {
-            console.log(facilities)
             var cateringDOM = document.getElementById("facility");
             cateringDOM.innerHTML = ""
             facilities.forEach((facility) => {
@@ -47,32 +45,58 @@ async function getFacilities() {
 }
 
 function submitForm() {
-    var id;
 
-    var catering = document.getElementById("catering");
-    var selectedCatering = catering.options[catering.selectedIndex];
-    var facility = document.getElementById("facility");
-    var selectedFacility = facility.options[facility.selectedIndex];
+    var eventName = document.getElementById("eventName");
+    var name = eventName.value;
 
+    var catering = document.getElementById("cateringSelector");
+    var facility = document.getElementById("facilitySelector");
 
-    fetch("http://localhost:8080/event/")
+    var select = catering.querySelector("select");
+    var selectedCatering = [].map.call(select.selectedOptions, function (option) {
+        return option.value;
+    }); 
+
+    var select1 = facility.querySelector("select");
+    var selectedFacility = [].map.call(select1.selectedOptions, function (option) {
+        return option.value;
+    }); 
+    console.log("http://localhost:8080/event/" + name)
+    fetch("http://localhost:8080/event/" + name, {
+        method: 'POST',
+        body: null, // data can be `string` or {object}!
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
         .then(res => {
             return res.json()
         })
         .then(res => {
-            id = res.id;
             return res.id;
         })
-        .then(eventId => {
-            console.log(eventId)
-            addEventToResources("http://localhost:3002/facility/", selectedFacility.value, eventId)
-            addEventToResources("http://localhost:8080/catering/", selectedCatering.value, eventId)
+        .then(async eventId => {
+            for (i in selectedFacility){
+                await addEventToResources("http://localhost:8080/facility/", selectedFacility[i], eventId)
+            }
+            for (index in selectedCatering){
+                await addEventToResources("http://localhost:8080/catering/", selectedCatering[index], eventId)
+            }
             return "succes";
+        })
+        .then( () => {
+            clearAllValues();
         }).catch((e) => {
             console.log(e)
         })
 
 
+}
+
+function clearAllValues(){
+    document.getElementById("catering").options.selectedIndex = -1;
+    document.getElementById("facility").options.selectedIndex = -1;
+    document.getElementById("eventName").value = "";
 }
 
 async function addEventToResources(url, resourceId, _eventId) {
@@ -96,82 +120,64 @@ async function getEvents() {
         .then(async res => {
             console.log(res)
             var eventList = [];
-            for (var i = 0; i < res.length; i++){
-                let facil = await fetch('http://localhost:8080/facility/' + res[i])
-                let cater = await fetch('http://localhost:8080/catering/' + res[i])
+            for (var i = 0; i < res.length; i++) {
+                let facil = await fetch('http://localhost:8080/facility/' + res[i].id)
+                let cater = await fetch('http://localhost:8080/catering/' + res[i].id)
                 let facility = await facil.json();
                 let catering = await cater.json();
-                eventList.push({ eventId: res[i], facilities: await facility, caterings: await catering });
-                GLOBAL_EVENTS[res[i]] = { facilities: await facility, caterings: await catering };
+                eventList.push({ event: {id : res[i].id, name: res[i].name }, facilities: await facility, caterings: await catering });
+                GLOBAL_EVENTS[res[i].id] = { name: res[i].name, facilities: await facility, caterings: await catering };
             }
             return eventList;
         })
         .then(events => {
-            console.log("g: " + GLOBAL_EVENTS)
             console.log(events)
             var table = document.getElementById("mytable");
             table.innerHTML = "";
-            table.innerHTML = "<tr> <th> eventId </th> <th> facility </th> <th> catering </th> <th> </th> </tr>"
+            table.innerHTML = "<tr> <th> name </th> <th> eventId </th> <th> facility </th> <th> catering </th> <th> </th> </tr>"
             //console.log(obj.events.events.length);
-            for (var i = 0; i < events.length; i++){
-                console.log(events[i])
-                table.innerHTML += "<tr> <td> " + events[i].eventId + "</td>" + "<td> " + events[i].facilities[0].name + "</td> <td> "+ events[i].caterings[0].name + "</td> <td> <center> <button onClick='getSingleEvent(" + events[i].eventId + ")'> show </button> </center> </td> </tr>"
-               /* for (var j = 0; j < events[i].facilities.length;j++){
-                    if(j === 0){
-                        table.innerHTML += "<tr> <td> " + events[i].eventId + "</td>" + "<td> " + events[i].facilities[0].name + "</td> <td> "+ events[i].caterings[0].name + "</td> <td> <center> <button> show </button> </center> </td>"
-                    }else{
-                        table.innerHTML += "<tr> <td> " + "" + "</td>" + "<td> " + events[i].facilities[j].name + "</td> <td> "+ events[i].caterings[j].name + "</td>"
-
-                    }
-                }
-                console.log(events[i].facilities.length)
-                */
+            for (var i = 0; i < events.length; i++) {
+                table.innerHTML += "<tr> <td>" + events[i].event.name + "</td> <td> " + events[i].event.id + "</td>" + "<td> " + events[i].facilities[0].name + "</td> <td> " + events[i].caterings[0].name + "</td> <td> <center> <button onClick='getSingleEvent(" + events[i].event.id + ")'> show </button> </center> </td> </tr>"
             }
-            //var keys = Object.keys(obj)
-            //console.log(keys)
-            //console.log(obj)
-  
+
         })
 }
 
-
-function redirect(eventid){
-    window.location.href = "event.html?eventid="+eventid;
-}
-
-function getSingleEvent(id){
+function getSingleEvent(id) {
     document.getElementById('singleevent').innerHTML = `
-    <div class="greyborder">
-    <a> Event id: </a> <a id="eventid"> </a> <br> <br>
+    <div class="greyborder" style="padding-bottom: 2%; !important">
+    <a> Name: </a> <a id="eventname"></a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a> Id: </a> <a id="eventid"> </a> <br> <br>
     <a>Facilities: </a>
-    <dl id="facilities" style="list-style-type:circle;">
-    </dl>
+    <table id="facilities">
+    </table>
     <br>
     <a>Caterings: </a>
-    <dl id="caterings">
-    </dl></div>`
+    <table id="caterings" style="padding-bottom: 5%;">
+    </table></div>`
 
-
+    var eventnameDOM = document.getElementById('eventname');
     var eventidDOM = document.getElementById('eventid');
     var facilitiesDOM = document.getElementById('facilities');
     var cateringDOM = document.getElementById('caterings');
-
     var event = GLOBAL_EVENTS[id];
-    console.log(event)
     eventidDOM.innerHTML = id
-    event.facilities.forEach( (facility) => {
-        facilitiesDOM.innerHTML += "<dt> name: " + facility.name + "</dt>  <dt> address: " + facility.address + "</dt> <dt> capacity: " + facility.capacity + "</dt>"
+    eventnameDOM.innerText = GLOBAL_EVENTS[id].name;
+    facilitiesDOM.innerHTML = "<tr> <th> name </th> <th> address </th> <th> capacity </th> </tr>"
+    event.facilities.forEach((facility) => {
+        facilitiesDOM.innerHTML += "<tr> <td>" + facility.name + "</td>  <td>" + facility.address + "</td> <td>" + facility.capacity + "</td> </tr>"
     })
-    event.caterings.forEach( (catering) => {
-        cateringDOM.innerHTML += "<dt> name: " + catering.name + "</dt>  <dt> address:" + catering.address + "</dt> <dt> food: " + catering.typeOfFood + "</dt>"
+    cateringDOM.innerHTML = "<tr> <th>name </th> <th> address </th> <th> food type </th> </tr>"
+    event.caterings.forEach((catering) => {
+        cateringDOM.innerHTML += "<td> " + catering.name + "</td>  <td> " + catering.address + "</td> <td> " + catering.typeOfFood + "</td>"
     })
 
-    console.log(GLOBAL_EVENTS[id])
+    console.log(GLOBAL_EVENTS[id] , id)
 
 
 
 }
 
-function getGlobalEvents(){
+function getGlobalEvents() {
     return GLOBAL_EVENTS;
 }
